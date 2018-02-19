806f78a88547370e35910c8bf45e5d12751de8e2
Ibrahim OGUZ <ibrahimoguz.ie@gmail.com>
2018-02-19 13:44:43 +0200

    Field title added.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderTitleField(field) {
+        return (
+            <div>
+                <input
+                    type="text"
+                    {...field.input} />
+            </div>
+        );
+    }
+
     render() {
         return (
             <div>
-                Create New Post!
+                <form>
+                    <h3>Create New Post!</h3>
+                    <Field name="title" component={this.renderTitleField} />
+                </form>
 
                 <div className="text-xs-right">
                     <Link className="btn btn-primary" to="/">
@@ -17,4 +31,6 @@ class PostsNew extends Component {
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
